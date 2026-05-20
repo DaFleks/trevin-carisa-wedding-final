43943312140e1220d68afc99d649255e200f6544
Alex <petropoulosalex@gmail.com>
2026-05-19 20:17:07 -0400

    removed initial background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased bg-black text-white`}>{children}</body>
+      <body className={`antialiased text-white`}>{children}</body>
     </html>
   );
 }
